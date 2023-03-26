4dbf1dedc2b15589116d7bfa333e04168e31248b
ThomasLonjon <lonjon.thomas@gmail.com>
2023-03-26 12:57:17 +0200

    added pokemonList

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,29 @@
+const pokemonList = [
+  {
+    name: "other",
+  },
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[0];
+
   return (
     <div>
-      <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="bulbasaur"
-        />
-      </figure>
+      {pokemon.name === "bulbasaur" ? (
+        <figure>
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        </figure>
+      ) : (
+        <p> ??? </p>
+      )}
     </div>
   );
 }
