9d0f6d88b37da1643e555d2150db99afceff6206
ThomasLonjon <lonjon.thomas@gmail.com>
2023-03-30 11:12:05 +0200

    added solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,51 @@ import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClickUp = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+
+  const handleClickDown = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
   return (
     <div className="App">
-      <PokemonCard pokemon={pokemon} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 && <button onClick={handleClickDown}>Previous</button>}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={handleClickUp}>Next</button>
+      )}
     </div>
   );
 }
-
-const pokemon = {
-  name: "bulbasaur",
-  imgSrc:
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-};
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -4,15 +4,16 @@ function PokemonCard(props) {
   return (
     <figure>
       <img src={props.pokemon.imgSrc} alt={props.pokemon.name} />
+      <p>{props.pokemon.name}</p>
     </figure>
   );
 }
 
-PokemonCard.propTypes = {
-  pokemon: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    imgSrc: PropTypes.imgSrc.isRequired,
-  }).isRequired,
-};
+// PokemonCard.propTypes = {
+//   pokemon: PropTypes.shape({
+//     name: PropTypes.string.isRequired,
+//     imgSrc: PropTypes.string.isRequired,
+//   }).isRequired,
+// };
 
 export default PokemonCard;
